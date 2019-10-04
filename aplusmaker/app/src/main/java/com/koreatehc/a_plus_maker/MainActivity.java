d50a274c8e46ef5f98238e6605fa9256283060c9
dongmin960903 <dongmin960903@gmail.com>
2019-10-04 22:39:42 +0900

    Add icon and color and button

--- aplusmaker/app/src/main/java/com/koreatehc/a_plus_maker/MainActivity.java
+++ aplusmaker/app/src/main/java/com/koreatehc/a_plus_maker/MainActivity.java
@@ -2,13 +2,36 @@ package com.koreatehc.a_plus_maker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button faqButton;
+    private Button studyButton;
+    private Button cheatButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void init(){
+        faqButton = (Button)findViewById(R.id.faq);
+        studyButton = (Button)findViewById(R.id.study);
+        cheatButton = (Button)findViewById(R.id.cheat_button);
+    }
+    public void onClickFAQButton(View view){
+        Intent intent = new Intent(getApplicationContext(),FaqActivity.class);
+        startActivity(intent);
+    }
+    public void onClickStudyButton(View view){
+        Intent intent = new Intent(getApplicationContext(),StudyActivity.class);
+        startActivity(intent);
+    }
+    public void onClickCheatButton(View view){
+        Intent intent = new Intent(getApplicationContext(),CheatActivity.class);
+        startActivity(intent);
     }
 }
---
+++ aplusmaker/app/src/main/java/com/koreatehc/a_plus_maker/StudyActivity.java
@@ -0,0 +1,14 @@
+package com.koreatehc.a_plus_maker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study);
+    }
+}
