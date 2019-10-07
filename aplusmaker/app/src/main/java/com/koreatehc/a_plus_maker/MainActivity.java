6b3f81cfe26f81b37b8d71db6ed7e4dcab424573
dongmin960903 <dongmin960903@gmail.com>
2019-10-08 02:26:04 +0900

    Add button icon img

--- aplusmaker/app/src/main/java/com/koreatehc/a_plus_maker/MainActivity.java
+++ aplusmaker/app/src/main/java/com/koreatehc/a_plus_maker/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
     }
     public void init(){
         faqButton = (Button)findViewById(R.id.faq);
