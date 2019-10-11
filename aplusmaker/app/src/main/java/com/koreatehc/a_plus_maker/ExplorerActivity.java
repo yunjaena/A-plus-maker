c1a8d602dfe6f3b1bfc5df380edbe9c129501a9d
khk1262 <khk1262@naver.com>
2019-10-11 14:54:04 +0900

    Add get file function

--- aplusmaker/app/src/main/java/com/koreatehc/a_plus_maker/ExplorerActivity.java
+++ aplusmaker/app/src/main/java/com/koreatehc/a_plus_maker/ExplorerActivity.java
@@ -1,4 +1,134 @@
 package com.koreatehc.a_plus_maker;
 
-public class ExplorerActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ExplorerActivity extends AppCompatActivity {
+
+    String mCurrent;
+    String mRoot;
+    TextView mCurrentTxt;
+    ListView mFileList;
+    ArrayAdapter<String> mAdapter;
+    ArrayList<String> arFiles;
+    ArrayList<String> arTemp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // 액션바(타이틀바) 감추기
+        setContentView(R.layout.activity_explorer);
+
+
+
+        mCurrentTxt = (TextView)findViewById(R.id.current);
+        mFileList = (ListView)findViewById(R.id.filelist);
+
+        arFiles = new ArrayList<String>();
+        arTemp = new ArrayList<String>();
+        mRoot = getApplicationContext().getExternalFilesDir(null).getAbsolutePath(); // SD카드 루트
+        mCurrent = mRoot;
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arFiles); // 어댑터 생성
+        mFileList.setAdapter(mAdapter); // 리스트뷰에 어댑터 연결
+        mFileList.setOnItemClickListener(mItemClickListener); // 리스트뷰에 리스너 연결
+
+        refreshFiles();
+    }
+
+    AdapterView.OnItemClickListener mItemClickListener =
+            new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String Name = arFiles.get(position); // 클릭된 위치의 값(경로)
+                    if (Name.startsWith("[") && Name.endsWith("]")) {
+                        Name = Name.substring(1, Name.length()-1); // 폴더이면 [] 제거
+                    }
+                    String Path = mCurrent + "/" + Name; // 경로+파일명 가공
+                    File f = new File(Path); // File 클래스 생성
+                    if (f.isDirectory()) {
+                        mCurrent = Path; // 폴더이면 mCurrent 갱신
+                        refreshFiles();
+                    } else {
+                        Toast.makeText(ExplorerActivity.this, arFiles.get(position), Toast.LENGTH_SHORT).show(); // 파일이면 ... 토스트 대신 필요한 작업을 지정하면 된다.
+                    }
+                }
+            };
+
+    public void mOnClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnroot:
+                if (mCurrent.compareTo(mRoot) != 0) { // 루트가 아니면 루트로 가기
+                    mCurrent = mRoot;
+                    refreshFiles();
+                }
+                break;
+
+            case R.id.btnup:
+
+                if (mCurrent.compareTo(mRoot) != 0) { // 루트가 아닐 때만
+                    int end = mCurrent.lastIndexOf("/"); // 마지막 폴더 구분자 위치
+                    String uppath = mCurrent.substring(0, end); // 구분자 앞까지만 살림(부모 폴더)
+                    mCurrent = uppath;
+                    refreshFiles();
+                }
+                break;
+        }
+    }
+
+    void refreshFiles() {
+        mCurrentTxt.setText(mCurrent); // 현재 path
+        arFiles.clear(); // 폴더리스트 배열 삭제
+        arTemp.clear(); // 파일리스트 배열 삭제
+        File current = new File(mCurrent); // 현재 path의 File 클래스
+        String[] files = current.list(); // 현재 path의 파일리스트(문자열) 배열
+
+        if (files != null) { // 파일이 있으면
+            String tmp = "";
+
+            for (int i = 0; i < files.length;i++) { // 배열 전체를 출력
+                tmp = files[i];
+
+                if (
+                        tmp.startsWith(".") ||
+                                tmp.startsWith("LG") ||
+                                tmp.endsWith(".db")
+                    // || (tmp.indexof(".db.") != -1)
+                ) continue; // 필터
+
+                String Path = mCurrent + "/" + files[i];
+                String Name = "";
+                File f = new File(Path);
+
+                if (f.isDirectory()) {
+                    Name = "[" + files[i] + "]"; // 폴더이면 [] 덧붙임
+                    arFiles.add(Name); // 폴더이면 arFiles 배열에 추가
+                } else {
+                    Name = files[i];
+                    arTemp.add(Name); // 파일이면 arTemp 배열에 추가
+                }
+                // arFiles.add(Name); // 파일리스트 배열에 추가
+            }
+            Collections.sort(arFiles); // 폴더리스트 정렬
+            Collections.sort(arTemp); // 파일리스트 정렬
+
+            Toast.makeText(this, "폴더:"+arFiles.size()+"/파일:"+arTemp.size(), Toast.LENGTH_SHORT).show();
+            arFiles.addAll(arTemp);
+            // Collections.sort(arFiles); // 파일리스트 정렬
+        }
+        mAdapter.notifyDataSetChanged(); // 리스트뷰 갱신
+    }
 }
--- aplusmaker/app/src/main/java/com/koreatehc/a_plus_maker/StudyActivity.java
+++ aplusmaker/app/src/main/java/com/koreatehc/a_plus_maker/StudyActivity.java
@@ -1,14 +1,101 @@
 package com.koreatehc.a_plus_maker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudyActivity extends AppCompatActivity {
 
+    public static final int FILE_SELECT_CODE = 1;
+    TextView modeText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // 액션바(타이틀바) 감추기
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study);
+
+        modeText =findViewById(R.id.study_mode);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(R.string.study_name);
+        }
+
+        Button modeButton = (Button)findViewById(R.id.study_mode);
+        modeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show_mode();
+            }
+        });
+
+        Button startButton = (Button)findViewById(R.id.study_start);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show_start();
+            }
+        });
+
+        Button explorerButton = (Button)findViewById(R.id.explorer);
+        explorerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFileChooser();
+            }
+        });
+    }
+
+    void showFileChooser() {
+
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        //intent.setType("*/*");      //all files
+        intent.setType("text/*");   //XML file only
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    void show_start(){
+        Toast.makeText(getApplicationContext(), "Button pressed!", Toast.LENGTH_SHORT).show();
+    }
+
+    void show_mode()
+    {
+        final List<String> ListItems = new ArrayList<>();
+        ListItems.add("일반모드학습");
+        ListItems.add("랜덤모드학습");
+        ListItems.add("깜박이모드학습");
+        ListItems.add("게임모드학습");
+        final CharSequence[] items =  ListItems.toArray(new String[ ListItems.size()]);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("학습 모드를 선택해주세요");
+
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int pos) {
+                String selectedText = items[pos].toString();
+                modeText.setText(selectedText);
+                Toast.makeText(StudyActivity.this, selectedText, Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
     }
 }
